7f62579a93fe8b50de3234a8d455692f852d04c2
ayu-gumgaonkar <ayushigumgaonkar8@gmail.com>
2024-05-04 16:50:16 +0530

    Applied Validation and added the Calender on the ValidFromDate and ValidUptoDate input.

--- CoditechLicenseApplication.ViewModel/ViewModel/GeneralMaster/ApplicationLicenseDetails/ApplicationLicenseDetailsViewModel.cs
+++ CoditechLicenseApplication.ViewModel/ViewModel/GeneralMaster/ApplicationLicenseDetails/ApplicationLicenseDetailsViewModel.cs
@@ -7,16 +7,21 @@ namespace Coditech.ViewModel
     public class ApplicationLicenseDetailsViewModel : BaseViewModel
     {
         public int ApplicationLicenseId { get; set; }
-        [Required]
+
+        [Required(ErrorMessage= "Client Name is required")]
         public string ClientName { get; set; }
-        [Required]
+
+        [Required(ErrorMessage="License Type is required")]
         public string LicenseType { get; set; }
-        [Required]
         public string DomainName { get; set; }
-        [Required]
         public string APIKey { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Please enter a valid date.")]
+        [DataType(DataType.Date)]
         public DateTime ValidFromDate { get; set; }
+
+        [Required(ErrorMessage = "Please enter a valid date.")]
+        [DataType(DataType.Date)]
         public DateTime ValidUptoDate { get; set; }
         public bool IsActive { get; set; }
     }
